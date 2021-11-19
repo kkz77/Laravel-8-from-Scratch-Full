ef6da01ce350f729f574b5b609a634908dfa174a
kkz77 <khantzawkaung2@gmail.com>
2021-11-19 20:51:39 +0630

    First commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>This is the title</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem eligendi et officiis perspiciatis porro
+           quibusdam quod tenetur voluptates. Deleniti doloribus excepturi hic, illo laudantium provident quaerat
+           quibusdam veritatis. Alias, perspiciatis?</p>
+    </article>
+    <article>
+        <h1><a href="post">My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem eligendi et officiis perspiciatis porro
+           quibusdam quod tenetur voluptates. Deleniti doloribus excepturi hic, illo laudantium provident quaerat
+           quibusdam veritatis. Alias, perspiciatis?</p>
+    </article>
+    <article>
+        <h1><a href="post">My Third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem eligendi et officiis perspiciatis porro
+           quibusdam quod tenetur voluptates. Deleniti doloribus excepturi hic, illo laudantium provident quaerat
+           quibusdam veritatis. Alias, perspiciatis?</p>
+    </article>
+</body>
+</html>
