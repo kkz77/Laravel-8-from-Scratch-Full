1d2971a1a7247cf3484fd2736d2c765481f4d3c3
kkz77 <khantzawkaung2@gmail.com>
2021-11-23 22:09:37 +0630

    ep-15 part-1

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,18 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>This is the title</title>
-    <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
+@extends('layout')
+@section('content')
     <article>
         <h1>{!! $post->title !!}</h1>
         <p>{!! $post->body !!}</p>
     </article>
     <a href="/">Go Back</a>
-</body>
-</html>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,20 +1,13 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>This is the title</title>
-    <link rel="stylesheet" href="css/app.css">
-</head>
-<body>
+@extends('layout')
+@section('header')
+    <h1>Otwo's Blog</h1>
+@endsection
+@section('content')
     @foreach ($posts as $post)
         <article>
             <h1><a href="/posts/{{$post->slug}}">{{ $post->title }}</a></h1>
             <p>{{ $post->excerpt }}</p>
         </article>
-    @endforeach
 
-</body>
-</html>
+    @endforeach
+@endsection
