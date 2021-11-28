1fbfb976012d6ebb41835b24511de771cbb32e2d
kkz77 <khantzawkaung2@gmail.com>
2021-11-29 02:22:10 +0630

    ep-26

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@
     use Illuminate\Support\Facades\Route;
 
     Route::get('/', function () {
-        return view('posts', ['posts' => Post::all()]);
+        return view('posts', ['posts' => Post::with('category')->get()]);
     });
 
     Route::get('posts/{post:slug}', function (Post $post) {
