fd779d50a008b379ba0c40a4c43b059e86c3d7ef
kkz77 <khantzawkaung2@gmail.com>
2021-12-22 21:04:55 +0630

    ep-47

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -5,16 +5,32 @@
             <form action="/register" method="POST" class="mx-auto w-80">
                 @csrf
                 <div class="items-center justify-center mx-auto my-6 w-72">
-                    <input type="text" name="name" class="h-8 w-72 p-2" placeholder="Name">
+                    <input type="text" name="name" class="h-8 w-72 p-2" placeholder="Name" value="{{old('name')}}"
+                           required>
+                    @error('name')
+                    <p class="my-1 text-red-500 text-xs">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="items-center justify-center mx-auto my-6 w-72">
-                    <input type="text" name="username" class="h-8 w-72 p-2" placeholder="Username">
+                    <input type="text" name="username" class="h-8 w-72 p-2" placeholder="Username"
+                           value="{{old('username')}}" required>
+                    @error('username')
+                    <p class="my-1 text-red-500 text-xs">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="items-center justify-center mx-auto my-6 w-72">
-                    <input type="password" name="password" class="h-8 w-72 p-2" placeholder="Password">
+                    <input type="password" name="password" class="h-8 w-72 p-2" placeholder="Password"
+                           value="{{old('password')}}" required>
+                    @error('password')
+                    <p class="my-1 text-red-500 text-xs">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="items-center justify-center mx-auto my-6 w-72">
-                    <input type="email" name="email" class="h-8 w-72 p-2" placeholder="Email">
+                    <input type="email" name="email" class="h-8 w-72 p-2" placeholder="Email" value="{{old('email')}}"
+                           required>
+                    @error('email')
+                    <p class="my-1 text-red-500 text-xs">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="flex justify-center">
                     <button class="bg-blue-500 justify-center px-4 py-2 rounded text-white">Submit</button>
