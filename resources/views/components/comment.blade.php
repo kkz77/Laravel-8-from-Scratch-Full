f1bdf91c9a9ecd86a4eaf90f665e1d53dda25797
kkz77 <khantzawkaung2@gmail.com>
2021-12-25 18:08:17 +0630

    ep-53

---
+++ resources/views/components/comment.blade.php
@@ -0,0 +1,20 @@
+<div class="flex bg-gray-200 border border-gray-300 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" width="60" height="60" alt="avatar"
+             class="border border-gray-300 rounded-full">
+    </div>
+    <div class="flex flex-col space-y-2">
+        <div>
+            <div class="font-bold">John Doe</div>
+            <div class="text-xs">Posted <span class="font-semibold">8 hours ago</span></div>
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias
+            consequatur
+            culpa
+            delectus et exercitationem, fuga ipsum iure labore nesciunt odit quasi rem
+            veritatis
+            voluptates.
+        </div>
+    </div>
+</div>
